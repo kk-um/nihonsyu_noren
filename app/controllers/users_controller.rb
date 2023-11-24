9b7e9eee2e71c15098036620197a8722e2158b4a
kk-um <143879591+kk-um@users.noreply.github.com>
2023-11-24 16:43:30 +0000

    layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @items = @user.items
-    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
   end
 
   def edit
