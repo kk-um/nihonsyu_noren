af93b7cf576d67859af0bfc6260efea51849e8fb
kk-um <143879591+kk-um@users.noreply.github.com>
2023-11-25 07:39:15 +0000

    layout

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,13 @@ class Item < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   validates :name, presence: true
-  
+
   def get_image(width,height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    image.variant(gravity: :center, resize:"#{width}x#{height}^", crop:"#{width}x#{height}+0+0").processed
   end
 
   # いいね機能で使用↓
@@ -28,7 +28,7 @@ class Item < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     ["name"]
   end
-  
+
   def self.ransackable_associations(auth_object = nil)
     ["user"]
   end
